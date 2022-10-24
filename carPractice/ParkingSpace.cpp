c7fa49ab001c3792bddd6da07d835a606839920e
cameron boyle <cameronboyle7894@gmail.com>
2022-10-24 20:41:12 +0100

    generated function definitions

--- carPractice/ParkingSpace.cpp
+++ carPractice/ParkingSpace.cpp
@@ -1,1 +1,26 @@
 #include "ParkingSpace.h"
+
+ParkingSpace::ParkingSpace()
+	: parkedCar()
+	, isParked(false)
+{
+	
+}
+
+ParkingSpace::ParkingSpace(Car newParkedCar, bool newIsParked)
+	: parkedCar(newParkedCar)
+	, isParked(newIsParked)
+{
+}
+
+void ParkingSpace::Park(Car)
+{
+}
+
+void ParkingSpace::Exit()
+{
+}
+
+void ParkingSpace::Print()
+{
+}
