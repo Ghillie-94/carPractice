1fdd445c5b2e266b7f69a9c897d70ea30beecb8f
cameron boyle <cameronboyle7894@gmail.com>
2022-10-24 20:40:41 +0100

    added variable and function declarations to ParkingSpace header

--- carPractice/ParkingSpace.h
+++ carPractice/ParkingSpace.h
@@ -1,7 +1,22 @@
 #pragma once
 
 #include <string>
+#include "car.h"
+
+
 class ParkingSpace
 {
+public:
+	ParkingSpace();
+	ParkingSpace(Car newParkedCar, bool newIsParked);
+
+	Car parkedCar;
+	bool isParked;
+
+	void Park(Car);
+	void Exit();
+	void Print();
+
+
 };
 
